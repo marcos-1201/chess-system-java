8bb91aef91fb787975798953ffcf5568e5f58061
marcos-1201 <mvi.santiago@gmail.com>
2021-01-25 21:53:57 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -31,6 +31,7 @@ public class ChessPosition {
 		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
 
+	@Override
 	public String toString() {
 		return "" + column + row;
 	}
